015048f1af7c19d67c3372bfcbbc9a30e1251d65
Medhane Mekonnen <medhanegirmay2@gmail.com>
2025-08-03 19:17:20 +0200

    simple metric class and datastorage implementation although the retreive data method needs fixing

--- src/main/java/com/systemmonitor/DataStorage.java
+++ src/main/java/com/systemmonitor/DataStorage.java
@@ -1,5 +1,30 @@
 package main.java.com.systemmonitor;
 
+import java.util.ArrayList;
+
 public class DataStorage {
+    // This class will handle data storage for the system monitor.
+    // It can be implemented to store data in memory, a database, or any other storage solution.
+
+    public void storeMetric(Metric metric) {
+        // Logic to save data
+        ArrayList<Metric> metrics = new ArrayList<>();
+        metrics.add(metric);
+    }
+
+    public String retrieveData(String key) {
+        // Logic to retrieve data
+        // For example, return a string representation of the metric
+        for (Metric metric : metrics) {
+            if (metric.getName().equals(key)) {
+                return metric.toString();
+            }
+        }
+        return null;
+    }
+
+    public void clearData() {
+        // Logic to clear stored data
+    }
     
 }
--- src/main/java/com/systemmonitor/Metric.java
+++ src/main/java/com/systemmonitor/Metric.java
@@ -1,5 +1,35 @@
 package main.java.com.systemmonitor;
 
 public class Metric {
+    private String name;
+    private double value;
+    private long timestamp;
+
+    public Metric(String name, double value, long timestamp) {
+        this.name = name;
+        this.value = value;
+        this.timestamp = timestamp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Metric{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                ", timestamp=" + timestamp +
+                '}';
+    }
     
 }
