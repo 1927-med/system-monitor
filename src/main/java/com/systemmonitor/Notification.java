839bc5cf82befad7c0a2e01b8eba7c1621c42f60
Medhane Mekonnen <medhanegirmay2@gmail.com>
2025-08-03 19:30:40 +0200

    notification class implementation

--- src/main/java/com/systemmonitor/Notification.java
+++ src/main/java/com/systemmonitor/Notification.java
@@ -1,5 +1,22 @@
 package main.java.com.systemmonitor;
 
 public class Notification {
+    private String name;
+    private String email;
+    
+    public Notification(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    
+    public void notifyAdmin(String message) {
+        // Logic to notify the admin
+        System.out.println("Admin notified: " + message);
+    }
+    
+    public void sendNotification(String message) {
+        // Logic to send a notification
+        System.out.println("Notification sent: " + message);
+    }
     
 }
