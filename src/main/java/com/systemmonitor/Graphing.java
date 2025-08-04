c06d8a9b60691772739dff3f6acddc340c05d271
Medhane Mekonnen <medhanegirmay2@gmail.com>
2025-08-04 03:39:25 +0200

    working on graphing gui using java fx

--- src/main/java/com/systemmonitor/Graphing.java
+++ src/main/java/com/systemmonitor/Graphing.java
@@ -1,14 +1,41 @@
 package main.java.com.systemmonitor;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
 
-public class  Graphing {
-     // Logic to create a graph
-     public void createGraph(String metricName, double[] values) {
-          // Logic to create a graph
-          System.out.println("Creating graph for " + metricName);
-          // You can use a library like JFreeChart to create a graph
-          // For now, just print the values
-          for (double value : values) {
-              System.out.println(value);
-          }
-      }
+public class  Graphing extends Application {
+     private double[] values;
+
+    public void createGraph(String title, double[] values) {
+        this.values = values;
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Graph");
+
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Values");
+
+        for (int i = 0; i < values.length; i++) {
+            series.getData().add(new XYChart.Data(i, values[i]));
+        }
+
+        lineChart.getData().add(series);
+
+        Scene scene = new Scene(lineChart, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
