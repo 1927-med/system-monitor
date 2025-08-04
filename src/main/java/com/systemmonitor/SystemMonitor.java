5bbce3dfbc255ad39b47c967e86df3e94b055c6b
Medhane Mekonnen <medhanegirmay2@gmail.com>
2025-08-04 02:11:04 +0200

    decided to not complicate the main class and deleted the unncessary logics so i will place them in other classes

--- src/main/java/com/systemmonitor/SystemMonitor.java
+++ src/main/java/com/systemmonitor/SystemMonitor.java
@@ -4,23 +4,8 @@ public class SystemMonitor {
     public static void main(String[] args) {
         System.out.println("System Monitor is running...");
         // Initialize components, start monitoring, etc.
-        // This is where the main logic of the system monitor would be implemented.
-        // For example, you might want to initialize the data storage, start collecting metrics,
-        // and handle alerts.
-        InMemoryDataStorage dataStorage = new InMemoryDataStorage();
-        Alerting alerting = new Alerting();
-
-         
-        
-         // Collect metrics
-         Metric metric = new Metric("CPU Usage", 50.0, System.currentTimeMillis());
-         dataStorage.storeMetric(metric);
-         
-         // Handle alerts
-         alerting.sendAlert("CPU usage is high!");
-            
-        
-        
+        MonitoringService monitoringService = new MonitoringService();
+        monitoringService.startMonitoring();
     }
     
 }
